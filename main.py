ddb2c1f08bc993c2f357706ee254950a494528d8
Fer Torres <fertorresmx@gmail.com>
2020-11-18 22:58:03 -0600

    Add function to list clients

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ clients = 'pablo,ricardo,'
 
 def create_client(client_name):
     global clients
+
     clients += client_name
+    _add_comma()
+
+
+def list_clients():
+    global clients
+
+    print(clients)
 
 
 def _add_comma():
@@ -13,5 +21,8 @@ def _add_comma():
 
 
 if __name__ == '__main__':
+    list_clients()
+
     create_client('David')
-    print(clients)
+
+    list_clients()
