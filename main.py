9d4336f95c61af1add24d2c0156ae76dd03f42a2
Fer Torres <fertorresmx@gmail.com>
2020-11-18 22:55:50 -0600

    Add function to add client

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 clients = 'pablo,ricardo,'
 
+
+def create_client(client_name):
+    global clients
+    clients += client_name
+
+
+def _add_comma():
+    global clients
+
+    clients +=','
+
+
 if __name__ == '__main__':
-    clients += 'david'
+    create_client('David')
     print(clients)
